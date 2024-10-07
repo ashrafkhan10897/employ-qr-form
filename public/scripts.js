d40e4388a126de27806c0af9544f9c343c37d6bc
Ashrafkhan <ashrafkhan10897@gmail.com>
2024-10-08 02:26:20 +0530

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const starsContainer = document.querySelector('.stars');
+  const starCount = 100;
+
+  for (let i = 0; i < starCount; i++) {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.animationDelay = `${Math.random() * 2}s`;
+    starsContainer.appendChild(star);
+  }
+});
+
 document.getElementById("employeeForm").addEventListener("submit", async (e) => {
   e.preventDefault();
 
